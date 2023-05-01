5a4be683253b95e83f401137941f14a160b7578f
GoldenStar1997 <golden.starr1997@gmail.com>
2023-05-01 04:07:00 +0100

    useState issure

--- src/components/popup/PortfolioModal.js
+++ src/components/popup/PortfolioModal.js
@@ -1,34 +1,28 @@
 import Modal from "./Modal";
 import store from "../../store";
-import { useEffect, useState } from "react";
 import Image from "next/image";
 
 const PortfolioModal = ({ item, close, open }) => {
-  const [backImage, setBackImage] = useState(store.portfolio[item]);
-  useEffect(() => {
-    const a = store.portfolio[item]
-    setBackImage(store.portfolio[item])
-
-  }, [item])
+  const data = store.portfolio[item]
   return (
     <Modal close={close} open={open} >
       <div className="portfolio_popup_details">
         <div className="top_image">
-          <Image src={"/img/portfolio/" + backImage.img + "/1.jpg"} alt="" width="1000px" height="600px" />
+          <Image src={"/img/portfolio/" + data.img + "/1.jpg"} alt="" width="1000px" height="600px" />
         </div>
         <div className="portfolio_main_title">
-          <h3>{backImage.tit}</h3>
+          <h3>{data.tit}</h3>
           <span>
-            <a href={backImage.url}>{backImage.url}</a>
+            <a href={data.url}>{data.url}</a>
           </span>
           <div />
         </div>
 
         <div className="text">
-          <p>{backImage.des}</p>
+          <p>{data.des}</p>
           <p> Built By: &nbsp;
             {
-              backImage.laf.map((ele, i) =>
+              data.laf.map((ele, i) =>
                 <span key={i}>{ele + ", "}</span>
               )
             }
@@ -40,10 +34,10 @@ const PortfolioModal = ({ item, close, open }) => {
             <li>
               <div className="list_inner">
                 <div className="image">
-                  <Image src={"/img/portfolio/" + backImage.img + "/2.jpg"} alt="" width="300px" height="200px"/>
+                  <Image src={"/img/portfolio/" + data.img + "/2.jpg"} alt="" width="300px" height="200px"/>
                   <a
                     className="edrea_tm_full_link zoom"
-                    href={"/img/portfolio/" + backImage.img + "/2.jpg"}
+                    href={"/img/portfolio/" + data.img + "/2.jpg"}
                   />
                 </div>
               </div>
@@ -51,10 +45,10 @@ const PortfolioModal = ({ item, close, open }) => {
             <li>
               <div className="list_inner">
                 <div className="image">
-                  <Image src={"/img/portfolio/" + backImage.img + "/3.jpg"} alt="" width="300px" height="200px"/>
+                  <Image src={"/img/portfolio/" + data.img + "/3.jpg"} alt="" width="300px" height="200px"/>
                   <a
                     className="edrea_tm_full_link zoom"
-                    href={"/img/portfolio/" + backImage.img + "/3.jpg"}
+                    href={"/img/portfolio/" + data.img + "/3.jpg"}
                   />
                 </div>
               </div>
